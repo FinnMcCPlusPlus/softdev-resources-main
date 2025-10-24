0bb9c2ac1df98fdd76d6166656311323c761a9f4
Finn Aloysious McCauley <finn.mccauley@gmail.com>
2025-10-24 13:19:10 -0700

    commiting

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ class MainScreen(Screen):
         """
 
         print("Call to stepper_motor_listener")
-        m_spd = int(self.ids.position.value) * 1000
+        m_spd = int(self.ids.position.value) * 100
         print ("Motor speed: " + str(m_spd))
         if int(self.ids.position.value) == 0:
             print("slider at 0, disabling motor")
